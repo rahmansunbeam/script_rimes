0c7afcb74209beca77431b93fb81b0c38f365e15
Sunbeam Rahman <sunbeam.rahman@live.com>
2023-03-30 15:27:14 +0600

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   script_emission_data.py

--- script_emission_data.py
+++ script_emission_data.py
@@ -16,7 +16,7 @@ arcpy.env.workspace = "E:\GIS_Files"
 arcpy.env.overwriteOutput = True
 
 # output_fishnet = r"E:\GIS_Files\BD_Fishnet.shp"
-output_fishnet = r"E:\GIS_Files\BD_GLEAM3_Emission_bfl_milk.shp"
+output_fishnet = r"D:\Data\LU_Emission\BD_Emission_all_1.shp"
 backup_csv = r"D:\Workspace\script_rimes\backup.csv"
 
 # Construct the API URL for this cell
@@ -43,11 +43,11 @@ with arcpy.da.SearchCursor(output_fishnet, ["OID@", "SHAPE@"]) as search_cur:
         xmax, ymax, xmin, ymin = round(extent_dd.XMax, 3), round(extent_dd.YMax, 3), round(extent_dd.XMin, 3), round(extent_dd.YMin, 3)
 
         # GLEAM3
-        url = f"https://io.apps.fao.org/gismgr/api/v1/GLEAM3/EMS/2/wms?request=GetFeatureInfo&tiled=true&query_layers=EMS&dim_src=ALL&bbox={xmin},{ymin},{xmax},{ymax}&format=image/png&dim_gas=CO2EQ&version=1.1.1&transparent=true&exceptions=application/vnd.ogc.se_xml&dim_comm={dim_comm_param}&dim_a_species={dim_a_species_param}&srs=EPSG:4326&service=WMS&layers=EMS&width=256&x=1&feature_count=101&y=195&styles=&info_format=application/json&height=256"
+        # url = f"https://io.apps.fao.org/gismgr/api/v1/GLEAM3/EMS/2/wms?request=GetFeatureInfo&tiled=true&query_layers=EMS&dim_src=ALL&bbox={xmin},{ymin},{xmax},{ymax}&format=image/png&dim_gas=CO2EQ&version=1.1.1&transparent=true&exceptions=application/vnd.ogc.se_xml&dim_comm={dim_comm_param}&dim_a_species={dim_a_species_param}&srs=EPSG:4326&service=WMS&layers=EMS&width=256&x=1&feature_count=101&y=195&styles=&info_format=application/json&height=256"
         
         # GLW 4: Gridded Livestock Density
         # url = f"'https://io.apps.fao.org/gismgr/api/v1/GLW4/D_DA/2/wms?request=GetFeatureInfo&tiled=true&query_layers=D_DA&bbox={xmin},{ymin},{xmax},{ymax}&format=image/png&version=1.1.1&transparent=true&exceptions=application/vnd.ogc.se_xml&dim_a_species={dim_a_species_param}&srs=EPSG:3857&service=WMS&layers=D_DA&width=256&x=31&feature_count=101&y=14&styles&info_format=application/json&height=256'"
-        url = f"https://io.apps.fao.org/gismgr/api/v1/GLW4/D_DA/2/wms?request=GetFeatureInfo&tiled=true&query_layers=D_DA&bbox={xmin},{ymin},{xmax},{ymax}&format=image/png&version=1.1.1&transparent=true&exceptions=application/vnd.ogc.se_xml&dim_a_species={dim_a_species_param}&srs=EPSG:3857&service=WMS&layers=D_DA&width=256&x=185&feature_count=101&y=199&styles=&info_format=application/json&height=256"
+        url = f"https://io.apps.fao.org/gismgr/api/v1/GLW4/D_DA/2/wms?request=GetFeatureInfo&tiled=true&query_layers=D_DA&bbox={xmin}%2C{ymin}%2C{xmax}%2C{ymax}&format=image%2Fpng&version=1.1.1&transparent=true&exceptions=application%2Fvnd.ogc.se_xml&dim_a_species={dim_a_species_param}&srs=EPSG%3A3857&service=WMS&layers=D_DA&width=256&x=117&feature_count=101&y=163&styles=&info_format=application%2Fjson&height=256"
         
         # Make the API request and parse the JSON response
         response = urllib.request.urlopen(url)
@@ -55,19 +55,21 @@ with arcpy.da.SearchCursor(output_fishnet, ["OID@", "SHAPE@"]) as search_cur:
 
         # Extract the emissions value from the JSON response
         species = json_data['features'][0]['properties']['Species']
-        param = json_data['features'][0]['properties']['Commodity']
-        emissions = json_data['features'][0]['properties']['Emissions (tons of COeq per km²)']
+        # param = json_data['features'][0]['properties']['Commodity']
+        # emissions = json_data['features'][0]['properties']['Emissions (tons of COeq per km²)']
+        density = json_data['features'][0]['properties']['Animal Density (head/km²)']
 
-        if emissions not in [None, "", 0.0]:
+        if density not in [None, "", 0.0]:
 
             print(json_data)
-            emissions_dict[search_row[0]] = [param, emissions]
+            # emissions_dict[search_row[0]] = [param, emissions]
+            emissions_dict[search_row[0]] = [density]
             # break
 
-with open(r"D:\Workspace\script_rimes\backup_{}_{}.csv".format(dim_a_species_param, dim_comm_param), "w", newline="") as csv_file:
+with open(r"D:\Workspace\script_rimes\backup_density_{}_{}.csv".format(dim_a_species_param, dim_comm_param), "w", newline="") as csv_file:
     writer = csv.writer(csv_file)
     # write header row
-    writer.writerow(["OID", "param", "emissions"])
+    writer.writerow(["OID", "param", "density"])
     # write data rows
     for oid, values in emissions_dict.items():
         row = [oid] + values
