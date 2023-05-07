85643b689f368aa7a99a4355f99e1f61d4f15138
Sunbeam Rahman <sunbeam.rahman@live.com>
2023-05-07 16:42:07 +0600

    On branch master Your branch is behind 'origin/master' by 3 commits, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	modified:   backup_density_CTL_MILK_PR.csv
    	modified:   script_emission_data.py

--- script_emission_data.py
+++ script_emission_data.py
@@ -16,7 +16,7 @@ arcpy.env.workspace = "E:\GIS_Files"
 arcpy.env.overwriteOutput = True
 
 # output_fishnet = r"E:\GIS_Files\BD_Fishnet.shp"
-output_fishnet = r"D:\Data\LU_Emission\BD_Emission_all_1.shp"
+output_fishnet = r"D:\Data\BD_Emission_all_1.shp"
 backup_csv = r"D:\Workspace\script_rimes\backup.csv"
 
 # Construct the API URL for this cell
@@ -45,11 +45,11 @@ with arcpy.da.SearchCursor(output_fishnet, ["OID@", "SHAPE@"]) as search_cur:
 
         # GLEAM3
         # url = f"https://io.apps.fao.org/gismgr/api/v1/GLEAM3/EMS/2/wms?request=GetFeatureInfo&tiled=true&query_layers=EMS&dim_src=ALL&bbox={xmin},{ymin},{xmax},{ymax}&format=image/png&dim_gas=CO2EQ&version=1.1.1&transparent=true&exceptions=application/vnd.ogc.se_xml&dim_comm={dim_comm_param}&dim_a_species={dim_a_species_param}&srs=EPSG:4326&service=WMS&layers=EMS&width=256&x=1&feature_count=101&y=195&styles=&info_format=application/json&height=256"
-        
+
         # GLW 4: Gridded Livestock Density
         # url = f"'https://io.apps.fao.org/gismgr/api/v1/GLW4/D_DA/2/wms?request=GetFeatureInfo&tiled=true&query_layers=D_DA&bbox={xmin},{ymin},{xmax},{ymax}&format=image/png&version=1.1.1&transparent=true&exceptions=application/vnd.ogc.se_xml&dim_a_species={dim_a_species_param}&srs=EPSG:3857&service=WMS&layers=D_DA&width=256&x=31&feature_count=101&y=14&styles&info_format=application/json&height=256'"
         url = f"https://io.apps.fao.org/gismgr/api/v1/GLW4/D_DA/2/wms?request=GetFeatureInfo&tiled=true&query_layers=D_DA&bbox={xmin}%2C{ymin}%2C{xmax}%2C{ymax}&format=image%2Fpng&version=1.1.1&transparent=true&exceptions=application%2Fvnd.ogc.se_xml&dim_a_species={dim_a_species_param}&srs=EPSG%3A3857&service=WMS&layers=D_DA&width=256&x=117&feature_count=101&y=163&styles=&info_format=application%2Fjson&height=256"
-        
+
         # Make the API request and parse the JSON response
         response = urllib.request.urlopen(url)
         json_data = json.loads(response.read().decode('utf-8'))
@@ -70,17 +70,17 @@ with arcpy.da.SearchCursor(output_fishnet, ["OID@", "SHAPE@"]) as search_cur:
 with open(r"D:\Workspace\script_rimes\backup_density_{}_{}.csv".format(dim_a_species_param, dim_comm_param), "w", newline="") as csv_file:
     writer = csv.writer(csv_file)
     # write header row
-    writer.writerow(["OID", "density"])
+    writer.writerow(["OID", "param", "density"])
     # write data rows
     for oid, values in emissions_dict.items():
-        row = [oid] + values
+        row = [oid] + dim_a_species_param + values
         writer.writerow(row)
 
 with arcpy.da.UpdateCursor(output_fishnet, ["OID@", "param", "emissions"]) as update_cur:
 
     for update_row in update_cur:
 
-         for key, values in emissions_dict.items():
+        for key, values in emissions_dict.items():
 
             try:
 
@@ -93,7 +93,7 @@ with arcpy.da.UpdateCursor(output_fishnet, ["OID@", "param", "emissions"]) as up
                     count += 1
 
             except StopIteration:
-                print("No more rows to search.")        
+                print("No more rows to search.")
 
 print("Number of rows updated:", count)
 
