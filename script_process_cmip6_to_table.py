7480d61b25fc660e832d487cb05054e8cdfe82d9
Sunbeam Rahman <sunbeam.rahman@live.com>
2023-05-29 10:32:01 +0600

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   README.md
    	modified:   backup_BFL_MEAT_PR.csv
    	modified:   backup_BFL_MILK_PR.csv
    	modified:   backup_CHK_EGGS_PR.csv
    	modified:   backup_CHK_MEAT_PR.csv
    	modified:   backup_CTL_MEAT_PR.csv
    	modified:   backup_CTL_MILK_PR.csv
    	modified:   backup_bufello.csv
    	modified:   backup_cattle.csv
    	modified:   backup_chicken.csv
    	modified:   backup_density_CTL_MILK_PR.csv
    	modified:   backup_density_GTS_MILK_PR.csv
    	modified:   backup_goat.csv
    	modified:   backup_sheep.csv
    	modified:   debug.log
    	modified:   script_emision_lu.py
    	modified:   script_emission_data.py
    	modified:   script_emission_data_merge.py
    	modified:   script_emission_trend.py
    	modified:   script_process_cmip6_csv.py
    	modified:   script_process_cmip6_nc.py
    	modified:   script_process_cmip6_nc_arcgis.py
    	modified:   script_process_cmip6_nc_defined_interval.py
    	new file:   script_process_cmip6_to_table.py
    
    Changes not staged for commit:
    	modified:   script_process_cmip6_csv.py
    	deleted:    script_process_cmip6_to_table.py
    
    Untracked files:
    	script_process_cmip6_download.py

---
+++ script_process_cmip6_to_table.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+__author__ = 'Sunbeam Rahman'
+__date__   = '22/05/2023 12:24:16'
+__email__  = 'sunbeam.rahman@live.com'
+
+## -------------------------------- ## 
+
