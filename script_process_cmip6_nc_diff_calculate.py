89e211687e80f64056b3d9b0cd73922b39821c5f
Sunbeam Rahman <sunbeam.rahman@live.com>
2023-08-23 13:59:24 +0600

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   script_process_cmip6_nc_diff_calculate.py

--- script_process_cmip6_nc_diff_calculate.py
+++ script_process_cmip6_nc_diff_calculate.py
@@ -8,52 +8,45 @@ __email__  = 'sunbeam.rahman@live.com'
 ## -------------------------------- ## 
 
 from pathlib import Path
+from itertools import zip_longest
 from datetime import datetime
-import pandas as pd
 import json
 
-json_historical_mon = r"D:\Data\Bangladesh_CMIP6_sublevels\ACCESS-CM2\historical\r1i1p1f1\ACCESS-CM2_hist_monthly_sum"
-json_current = r"D:\Data\Bangladesh_CMIP6_sublevels\ACCESS-CM2\historical\r1i1p1f1\ACCESS-CM2_ssp245_avg\ACCESS-CM2_monthly"
-json_output = Path(r"D:\Data\Bangladesh_CMIP6_sublevels\ACCESS-CM2\historical\r1i1p1f1\output")
+json_historical_mon = r"D:\DOCUMENTS\RIMES\NETCDF_CMIP6_sublevels\ACCESS-CM2_Historical_monthly_sum"
+json_curr = r"D:\DOCUMENTS\RIMES\NETCDF_CMIP6_sublevels\ACCESS-CM2_monthly"
+json_output = Path(r"D:\DOCUMENTS\RIMES\NETCDF_CMIP6_sublevels\output")
 
-files_base = [file for file in Path(json_historical_mon).glob("**\*.json") if 'div' not in file.name]
-files_current = [file for file in Path(json_current).glob("**\*.json") if 'div' not in file.name]
+files_historical = [file for file in Path(json_historical_mon).glob("**\*.json")]
+files_current = [file for file in Path(json_curr).glob("**\*.json")]
 
-for file_base in files_base:
-    for file_current in files_current:
+for file_historical, file_current in zip_longest(files_historical, files_current):
 
-        result_dict = {}
+    result_dict = {}
 
-        with file_base.open() as f:
-            json_base = eval(f.read())
+    with file_historical.open() as f:
+        json_hist = json.load(f)
 
-        with file_current.open() as f:
-            json_current = eval(f.read())
+    with file_current.open() as f:
+        json_curr = json.load(f)
 
-        if 'pr' in file_base.name and 'pr' in file_current.name:
+    for key_hist, value_hist in json_hist.items():
 
-            diff_dict = {}
-            # key_list = [str(i) for i in range(64)]
+        for key_curr, value_curr in json_curr.items():
 
-            for key_base, value_base in json_base.items():
-                for key_current, value_current in json_current.items():
-                    if key_base != 'station' and key_current != 'station':
-                        key_base_month = datetime.strptime(key_base.split('_')[-1], '%b')
-                        key_curr_year = datetime.strptime(key_current.split('_')[1], '%Y')
-                        key_curr_month = datetime.strptime(key_current.split('_')[-1], '%b')
-                        
-                        if key_base_month == key_curr_month:
-                            diff_values = [value_current[month] - value_base[month] for month in value_current]   
-                            d1 = dict()
-                            for i in range(len(diff_values)):
-                                d1[str(i)] = diff_values[i]                     
-                            result_dict[key_current] = d1                            
-                            print(result_dict)
-                            break
+            if 'station' in (key_hist, key_curr):
+                continue
 
-            json_output.mkdir(parents=True, exist_ok=True)
+            key_hist_month = datetime.strptime(key_hist.split('_')[-1], '%b')
+            key_curr_year = datetime.strptime(key_curr.split('_')[1], '%Y')
+            key_curr_month = datetime.strptime(key_curr.split('_')[-1], '%b')
+            
+            if key_hist_month == key_curr_month:
+                diff_values = [value_hist[month] - value_curr[month] for month in value_curr]
+                result_dict[key_curr] = {str(i): diff for i, diff in enumerate(diff_values)}
 
-            output_file_name = json_output / (file_current.stem + "_diff.json")
-            with output_file_name.open('w') as f:
-                f.write(json.dumps(result_dict, indent=4))
+    json_output.mkdir(parents=True, exist_ok=True)
+
+    output_file_name = json_output / (file_current.stem + "_hist_to_projected_diff.json")
+    with output_file_name.open('w') as f:
+        json.dump(result_dict, f, indent=4)
 
